c9ed35577b9293f1c609c42aec150f59ae4b15da
LiuCaiHe <at@lang.so>
2016-10-09 10:47:02 +0800

    add weChat

---
+++ pages/detail/detail.js
@@ -0,0 +1,185 @@
+
+const util = require( '../../utils/util.js' );
+
+Page( {
+    data: {
+        // text:"这是一个页面"
+        data: [],
+        databody: null,
+
+        winHeight: 0,
+
+        // 弹窗
+        modalHidden: true,
+        modalValue: null,
+
+        /**
+         * 分享配置
+         */
+        shareShow: 'none',
+        shareOpacity: {},
+        shareBottom: {},
+
+    },
+    onLoad: function( options ) {
+        // 页面初始化 options 为页面跳转所带来的参数
+        var that = this
+        var id = options.id;
+
+
+        // 请求数据
+        util.AJAX( "news/" + id, function( res ) {
+
+            var arr = res.data;
+            var body = arr.body;
+            body = body.match( /<p>.*?<\/p>/g );
+            var ss = [];
+            for( var i = 0;i < body.length;i++ ) {
+
+                ss[ i ] = /<img.*?>/.test( body[ i ] );
+
+                if( ss[ i ] ) {
+                    body[ i ] = body[ i ].match( /(http:|https:).*?\.(jpg|jpeg|gif|png)/ );
+                } else {
+                    body[ i ] = body[ i ].replace( /<p>/g, '' )
+                        .replace( /<\/p>/g, '' )
+                        .replace( /<strong>/g, '' )
+                        .replace( /<\/strong>/g, '' )
+                        .replace( /<a.*?\/a>/g, '' )
+                        .replace( /&nbsp;/g, ' ' )
+                        .replace( /&ldquo;/g, '"' )
+                        .replace( /&rdquo;/g, '"' );
+                }
+            }
+
+            // 然后重新写入数据
+            that.setData( {
+                data: arr,
+                databody: body
+            });
+
+        });
+
+        /**
+         * 获取系统信息
+         */
+        wx.getSystemInfo( {
+
+            success: function( res ) {
+                that.setData( {
+                    winWidth: res.windowWidth,
+                    winHeight: res.windowHeight
+                });
+            }
+        });
+
+
+    },
+    /**
+     * 显示分享
+     */
+    showShare: function( e ) {
+
+        // 创建动画
+        var animation = wx.createAnimation( {
+            duration: 100,
+            timingFunction: "ease",
+        })
+        this.animation = animation;
+
+        var that = this;
+        that.setData( {
+            shareShow: "block",
+        });
+
+        setTimeout( function() {
+            that.animation.bottom( 0 ).step();
+            that.setData( {
+                shareBottom: animation.export()
+            });
+        }.bind( this ), 400 );
+
+        // 遮罩层
+        setTimeout( function() {
+            that.animation.opacity( 0.3 ).step();
+            that.setData( {
+                shareOpacity: animation.export()
+            });
+        }.bind( this ), 400 );
+
+    },
+
+    /**
+     * 关闭分享
+     */
+    shareClose: function() {
+        // 创建动画
+        var animation = wx.createAnimation( {
+            duration: 0,
+            timingFunction: "ease"
+        })
+        this.animation = animation;
+
+        var that = this;
+
+        setTimeout( function() {
+            that.animation.bottom( -210 ).step();
+            that.setData( {
+                shareBottom: animation.export()
+            });
+        }.bind( this ), 500 );
+
+        setTimeout( function() {
+            that.animation.opacity( 0 ).step();
+            that.setData( {
+                shareOpacity: animation.export()
+            });
+        }.bind( this ), 500 );
+
+        setTimeout( function() {
+            that.setData( {
+                shareShow: "none",
+            });
+        }.bind( this ), 1500 );
+    },
+
+    /**
+     * 点击分享图标弹出层
+     */
+    modalTap: function( e ) {
+        var that = this;
+        that.setData( {
+            modalHidden: false,
+            modalValue: e.target.dataset.share
+        })
+    },
+    
+    /**
+     * 关闭弹出层
+     */
+    modalChange: function( e ) {
+        var that = this;
+        that.setData( {
+            modalHidden: true
+        })
+    },
+
+    onReady: function() {
+        // 页面渲染完成
+        // 修改页面标题
+        wx.setNavigationBarTitle( {
+            title: this.data.data.title
+        })
+
+
+    },
+    onShow: function() {
+        // 页面显示
+    },
+    onHide: function() {
+        // 页面隐藏
+    },
+    onUnload: function() {
+        // 页面关闭
+    }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,184 @@
+
+const util = require( '../../utils/util.js' );
+
+Page( {
+    data: {
+
+        /**
+         * 页面配置
+         */
+        winWidth: 0,
+        winHeight: 0,
+
+        currentTab: 0,
+
+        // 请求的数据
+        datalist: [],
+
+        dataListDateCurrent: 0,      // 当前日期current
+        dataListDateCount: 0,      // 请求次数
+
+        // 显示加载更多 loading
+        hothidden: true,
+
+        // loading
+        hidden: true,
+
+        /**
+         * 滑动面板参数配置
+         */
+        indicatorDots: false,    // 是否显示面板指示点
+        autoplay: false,    // 是否自动切换
+        interval: 5000,     // 自动切换时间间隔
+        duration: 1000,     // 滑动动画时长
+
+    },
+
+    /** 
+     * 页面初始化
+     * options 为页面跳转所带来的参数
+     */
+    onLoad: function( options ) {
+        var that = this;
+
+        /**
+         * 获取系统信息
+         */
+        wx.getSystemInfo( {
+
+            success: function( res ) {
+                that.setData( {
+                    winWidth: res.windowWidth,
+                    winHeight: res.windowHeight
+                });
+            }
+
+        });
+
+
+        /**
+         * 显示 loading
+         */
+        that.setData( {
+            hidden: false
+        });
+
+        // 请求数据
+        util.AJAX( "news/latest", function( res ) {
+
+            var arr = res.data;
+            var format = util.getFormatDate( arr.date );
+
+            // 格式化日期方便加载指定日期数据
+            // 格式化日期获取星期几方便显示
+            arr[ "dateDay" ] = format.dateDay;
+            // 获取当前现有数据进行保存
+            var list = that.data.datalist;
+
+            // 然后重新写入数据
+            that.setData( {
+                datalist: list.concat( arr ),
+                dataListDateCurrent: arr.date,    // 当前日期
+                dataListDateCount: 1
+            });
+        });
+    },
+    onReady: function() {
+        // 页面渲染完成
+        var that = this;
+
+        // 数据加载完成后 延迟隐藏loading
+        setTimeout( function() {
+            that.setData( {
+                hidden: true
+            })
+        }, 500 );
+
+
+    },
+    onShow: function() {
+        // 页面显示
+    },
+    onHide: function() {
+        // 页面隐藏
+    },
+    onUnload: function() {
+        // 页面关闭
+    },
+
+
+    /**
+     * 事件处理
+     * scrolltolower 自动加载更多
+     */
+    scrolltolower: function( e ) {
+
+        var that = this;
+
+        // 加载更多 loading
+        that.setData( {
+            hothidden: true
+        })
+
+        var currentDate = this.data.dataListDateCurrent;
+
+        // 如果加载数据超过10条
+        if( this.data.dataListDateCount >= 8 ) {
+
+            // 加载更多 loading
+            that.setData( {
+                hothidden: false
+            });
+
+        } else {
+
+            /**
+             * 发送请求数据
+             */
+            util.AJAX( "news/before/" + currentDate, function( res ) {
+
+                var arr = res.data;
+                var format = util.getFormatDate( arr.date );
+
+                // 格式化日期方便加载指定日期数据
+                // 格式化日期获取星期几方便显示
+                arr[ "dateDay" ] = format.dateDay;
+
+                // 获取当前数据进行保存
+                var list = that.data.datalist;
+                // 然后重新写入数据
+                that.setData( {
+                    datalist: list.concat( arr ),                              // 存储数据
+                    dataListDateCurrent: arr.date,
+                    dataListDateCount: that.data.dataListDateCount + 1      // 统计加载次数
+                });
+            });
+        }
+    },
+    /**
+     * 滑动切换tab
+     */
+    bindChange: function( e ) {
+
+        var that = this;
+        that.setData( { currentTab: e.detail.current });
+
+    },
+    /**
+     * 点击tab切换
+     */
+    swichNav: function( e ) {
+
+        var that = this;
+
+        if( this.data.currentTab === e.target.dataset.current ) {
+            return false;
+        } else {
+            that.setData( {
+                currentTab: e.target.dataset.current
+            })
+        }
+
+
+    },
+})
